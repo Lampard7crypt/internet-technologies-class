23a4bbbcd61dd92fc1eba700f07494a6d1e9f70d
Tyler <tk654001@gmail.com>
2026-01-15 13:57:00 +0300

    Arranging code blocks

--- assignment.js
+++ assignment.js
@@ -1,15 +1,8 @@
-// 1. Select the DOM elements we need to interact with
 const pythonCheckbox = document.getElementById('pythonCheck');
 const jsCheckbox = document.getElementById('jsCheck');
 const submitButton = document.getElementById('submit-btn');
 const resultText = document.getElementById('resultDisplay');
 
-submitButton.addEventListener('click', function (e) {
-    e.preventDefault(); // stop form submission
-    // existing checkbox logic.../
-});
-
-// 2. Add an event listener to the button
 submitButton.addEventListener('click', function () {
 
     const isPythonChecked = pythonCheckbox.checked;
@@ -25,4 +18,9 @@ submitButton.addEventListener('click', function () {
     } else {
         resultText.textContent = "Please select at least one skill.";
     }
+});
+
+
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
 });
